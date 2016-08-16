f7f67c8d28b6a12d5ba8793192f57f1dc7b33ed9
AnttiLatomaa <antti.j.latomaa@gmail.com>
2016-08-16 10:29:44 +0200

    Lade till Angular JS

--- PersonalSystem/PersonalSystem/App_Start/BundleConfig.cs
+++ PersonalSystem/PersonalSystem/App_Start/BundleConfig.cs
@@ -8,6 +8,9 @@ namespace PersonalSystem
 		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
 		public static void RegisterBundles(BundleCollection bundles)
 		{
+            bundles.Add(new ScriptBundle("~/bundles/angularJS").Include(
+                "~/Scripts/angular.min.js"));
+
 			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
 						"~/Scripts/jquery-{version}.js"));
 

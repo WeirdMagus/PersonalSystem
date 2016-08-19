34565abbed5b3236c6172e26d5fe1455ff302fb8
AnttiLatomaa <antti.j.latomaa@gmail.com>
2016-08-19 15:44:42 +0200

    Pillade mer med Ansökningar

--- PersonalSystem/PersonalSystem/Repositories/ApplicationRepository.cs
+++ PersonalSystem/PersonalSystem/Repositories/ApplicationRepository.cs
@@ -17,38 +17,80 @@ namespace PersonalSystem.Repositories
             _db = ApplicationDbContext.Create();
         }
 
-        public Application Create(Application application)
+        private Application Create(Application application)
         {
             if (application != null || String.IsNullOrWhiteSpace(application.CoverLetter) || application.Applicant != null || application.Vacancy != null)
                 return application;
             return null;
         }
-        public Application Get(int? id)
+        private Application Get(int? id)
         {
             return _db.Applications.Single(a => a.Id == id);
         }
-
-        public List<Application> GetAllFromVacancy(int? id)
+        private List<Application> GetAllAppsForVacancy(int? id) // Hämtar alla ansökningar för specifik annons
         {
             return _db.Applications.Where(a => a.Vacancy.Id == id).ToList();
         }
-
-        public bool Update(Application app)
+        private bool Update(Application app)
         {
             _db.Entry(app).State = EntityState.Modified;
             _db.SaveChanges();
             return true;
         }
-        public void Delete(int? id)
+        private void Delete(int? id)
         {
             Application app = Get(id);
             Delete(app);
         }
-        public void Delete(Application app)
+        private void Delete(Application app)
         {
             _db.Applications.Remove(app);
             _db.SaveChanges();
         }
 
+        public void CreateApp(Application app)
+        {
+            if (app.Vacancy != null && app.CoverLetter != null && app.CoverLetter != null && app.Applicant != null)
+            {
+                this.Create(app);
+            }
+        }
+        public Application GetApp(int? id)
+        {
+            if(id != null)
+            {
+               return this.Get(id);
+            }
+            return null;
+        }
+        public List<Application> GetAppsForVacancy(int? id)
+        {
+            if(id != null)
+            {
+                return this.GetAllAppsForVacancy(id);
+            }
+            return null;
+        }
+        public void DeleteApp(Application app)
+        {
+            if(app != null)
+            {
+                this.Delete(app);
+            }
+        }
+        public void UpdateApp(Application app)
+        {
+            if(app != null)
+            {
+                this.Update(app);
+            }
+        }
+        public void DeleteApp(int? id)
+        {
+            if(id != null)
+            {
+                this.Delete(id);
+            }
+        }
     }
 }

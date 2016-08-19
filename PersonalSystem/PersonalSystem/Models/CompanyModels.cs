422f454f58c8e41c3bf75931ee78b6c44f97a15b
WeirdMagus <Weird_Magus@Hotmail.com>
2016-08-19 17:28:19 +0200

    Database updated to be generic

--- PersonalSystem/PersonalSystem/Models/CompanyModels.cs
+++ PersonalSystem/PersonalSystem/Models/CompanyModels.cs
@@ -7,77 +7,129 @@ using System.Web;
 
 namespace PersonalSystem.Models
 {
-	public class Company
+	public abstract class BaseEntity
 	{
 		public int Id { get; set; }
+	}
+	public class Company : BaseEntity
+	{
+		//public int Id { get; set; }
 		[Required]
 		[Index(IsUnique = true)]
 		[StringLength(50, ErrorMessage = "{0} needs to be at least {1} characters", MinimumLength = 3)]
 		public string Name { get; set; }
 		public virtual ApplicationUser Admin { get; set; }
-		public virtual IEnumerable<Vacancy> Vacancies { get; set; }
-		public virtual IEnumerable<News> News { get; set; }
+		public virtual ICollection<Department> Departments { get; set; }
+		public virtual ICollection<Vacancy> Vacancies { get; set; }
+		public virtual ICollection<News> News { get; set; }
+
+		public Company()
+		{
+			Departments = new List<Department> { new Department() };
+			Vacancies = new List<Vacancy>();
+			News = new List<News>();
+		}
 	}
 
-	public class Department
+	public class Department : BaseEntity
 	{
-		public int Id { get; set; }
+		//public int Id { get; set; }
 		[Required]
 		[Index(IsUnique = true)]
 		[StringLength(50, ErrorMessage = "{0} needs to be at least {1} characters", MinimumLength = 3)]
 		public string Name { get; set; }
 		public virtual Company Company { get; set; }
-		public virtual IEnumerable<Group> Grouper { get; set; }
+		public virtual ICollection<Group> Groups { get; set; }
+
+		public Department()
+		{
+			Groups = new List<Group> { new Group() };
+		}
 	}
 
-	public class Group
+	public class Group : BaseEntity
 	{
-		public int Id { get; set; }
+		//public int Id { get; set; }
 		[Required]
 		[Index(IsUnique = true)]
 		[StringLength(50, ErrorMessage = "{0} needs to be at least {1} characters", MinimumLength = 3)]
 		public string Name { get; set; }
 		public virtual Department Department { get; set; }
 		public virtual Schedule Schedule { get; set; }
-		public virtual IEnumerable<ApplicationUser> Users { get; set; }
+		public virtual ICollection<ApplicationUser> Users { get; set; }
 	}
 
-	public class Schedule
+	public class DailySchedule
 	{
-		public int Id { get; set; }
+		[NotMapped]
+		public DayOfWeek day { get; set; }
+		public TimeSpan StartTime { get; set; }
+		public TimeSpan EndTime { get; set; }
+	}
+
+	public class Schedule : BaseEntity
+	{
+		//public int Id { get; set; }
 		public string Name { get; set; }
+		public DailySchedule Monday { get; set; }
+		public DailySchedule Tuesday { get; set; }
+		public DailySchedule Wednesday { get; set; }
+		public DailySchedule Thursday { get; set; }
+		public DailySchedule Friday { get; set; }
+		public DailySchedule Saturday { get; set; }
+		public DailySchedule Sunday { get; set; }
+		[NotMapped]
+		public List<DailySchedule> FullSchedule
+		{
+			get
+			{
+				return GetFullSchedule();
+			}
+		}
+
+		private List<DailySchedule> GetFullSchedule()
+		{
+			Monday.day = DayOfWeek.Monday;
+			Tuesday.day = DayOfWeek.Tuesday;
+			Wednesday.day = DayOfWeek.Wednesday;
+			Thursday.day = DayOfWeek.Thursday;
+			Friday.day = DayOfWeek.Friday;
+			Saturday.day = DayOfWeek.Saturday;
+			Sunday.day = DayOfWeek.Sunday;
+			return new List<DailySchedule>
+				{
+					Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+				};
+		}
 	}
 
-	public class News
+	public class News : BaseEntity
 	{
-		public int Id { get; set; }
 		public string Name { get; set; }
 		public virtual Company Company { get; set; }
 		public bool IsInternal { get; set; }
 		public string Text { get; set; }
 	}
 
-	public class Vacancy
+	public class Vacancy : BaseEntity
 	{
-		public int Id { get; set; }
-        [Required]
-        [StringLength(50, ErrorMessage = "{0} needs to be at least {1} characters", MinimumLength = 3)]
-        public string JobTitle { get; set; }
-        public virtual IEnumerable<Application> Applications { get; set; }
+		[Required]
+		[StringLength(50, ErrorMessage = "{0} needs to be at least {1} characters", MinimumLength = 3)]
+		public string JobTitle { get; set; }
+		public virtual ICollection<Application> Applications { get; set; }
 	}
 
-	public class Application
+	public class Application : BaseEntity
 	{
-		public int Id { get; set; }
-        [Required]
-        [StringLength(5000, ErrorMessage = "{0} needs to be at least {1} characters", MinimumLength = 3)]
-        public string CoverLetter { get; set; }     // Personligt brev
+		[Required]
+		[StringLength(5000, ErrorMessage = "{0} needs to be at least {1} characters", MinimumLength = 3)]
+		public string CoverLetter { get; set; }     // Personligt brev
 
-        [Required]
+		[Required]
 		public virtual Vacancy Vacancy { get; set; }
 
-        [Required]
-        public virtual ApplicationUser Applicant { get; set; }
+		[Required]
+		public virtual ApplicationUser Applicant { get; set; }
 	}
 
 }
